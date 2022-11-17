889961e112ed418cf509c28b8527cabc07623cf3
ABazh <alexbalex1@yandex.ru>
2022-11-18 00:19:19 +0400

    It works with Runnable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,11 +20,11 @@ public class Main {
         File[] files1 = new File[middle];
         System.arraycopy(files, 0, files1,0, files1.length);
         ImageResizer resizer1 = new ImageResizer(files1, newWidth, dtsFolder,start);
-        resizer1.start();
+        new Thread(resizer1).start();
 
         File[] files2 = new File[files.length - middle];
         System.arraycopy(files, middle, files2, 0, files2.length);
         ImageResizer resizer2 = new ImageResizer(files2, newWidth, dtsFolder,start);
-        resizer2.start();
+        new Thread(resizer2).start();
     }
 }
